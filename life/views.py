40907b2a5beaf42d58d71e23775105563e39be9d
emiliehasselmann <emiliehasselmann@berkeley.edu>
2018-07-22 03:05:28 -0400

    add grocery and pantry list done

--- life/views.py
+++ life/views.py
@@ -3,6 +3,7 @@ from django.core import serializers
 from life.models import *
 from django.core.serializers import serialize
 import json
+import random
 
 
 def index(request):
@@ -11,11 +12,18 @@ def index(request):
     if request.method == "POST":
         data = json.loads(request.body.decode('utf-8'))
         print(data)
-        if(data['food'] and data['quantity']):
-            new_food = Food(name = data['food'], price = 2.99)
+        
+        if(data['listType'] == 'groceryList' and data['food'] and data['quantity']):
+            new_food = Food(name = data['food'], price = random.randint(1,10))
             new_food.save()
             new_grocery = GroceryList(food = new_food, quantity=data['quantity'])
             new_grocery.save()
+        if(data['listType'] == 'pantryList' and data['food'] and data['quantity']):
+            new_food = Food(name = data['food'], price = random.randint(1,10))
+            new_food.save()
+            new_grocery = PantryList(food = new_food, quantity=data['quantity'])
+            new_grocery.save()    
+            
         return HttpResponse("")
     else:
         grocery_items = GroceryList.objects.all()
