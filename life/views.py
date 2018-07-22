4e175759e25d1b9caa69d34ec48d48b50777dc72
emiliehasselmann <emiliehasselmann@berkeley.edu>
2018-07-21 23:57:16 -0400

    getting json from html

--- life/views.py
+++ life/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.core import serializers
 from life.models import *
+from django.core.serializers import serialize
+import json
+
 
 def index(request):
     #all_foods = Food.objects.all()
     #return render(request, 'life/index.html', {"foods": all_foods})
-    grocery_items = GroceryList.objects.all()
-    pantry_items = PantryList.objects.all()
-    return render(request, 'life/index.html', {"grocerylist": grocery_items, "pantrylist": pantry_items})
+    if request.method == "POST":
+#         print(json.loads(request.body))
+        print(json.loads(request.body.decode('utf-8')))
+
+#         myResponse.json()
+        return HttpResponse("")
+    else:
+        grocery_items = GroceryList.objects.all()
+        pantry_items = PantryList.objects.all()
+        return render(request, 'life/index.html', {"grocerylist": grocery_items, "pantrylist": pantry_items})
   
   
